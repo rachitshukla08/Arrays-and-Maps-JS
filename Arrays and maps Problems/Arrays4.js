6447444642c68eb09a1a56775766d6c666bf1b44
unknown <rachitshukla08@gmail.com>
2020-11-07 08:47:54 +0530

    Added: Array practice problems for day 33

---
+++ Arrays and maps Problems/Arrays4.js
@@ -0,0 +1,12 @@
+let numberArray = new Array();
+numberArray.push(5);
+numberArray.push(6);
+numberArray.push(-11);
+console.log(numberArray);
+let sum =0;
+function findSum(sum,num){
+    sum+=num;
+    return sum;
+}
+numberArray.reduce(findSum,0);
+console.log("Sum is "+sum);
